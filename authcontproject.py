6d671c2a2b6506d69e0e18a7d9f014005aba51bb
OKPETE TOMJAY <81090931+tomjay1@users.noreply.github.com>
2021-05-05 02:58:00 +0100

    Add files via upload
    CONTINUATION OF THE AUTHENTICATION PROJECTS

--- authcontproject.py
+++ authcontproject.py
@@ -13,31 +13,37 @@ import random
 database = {} #dictonary
 
 def init():
-    isValidOptionSelected =False
-    print ('welcome to bankphp')
     
-    while isValidOptionSelected ==False:
+    print ('welcome to bankphp')
+       
+    haveaccount = int(input('do you have an account with us: 1 (yes) 2 (no) \n'))
         
-        haveaccount = int(input('do you have an account with us: 1 (yes) 2 (no) \n'))
-        
-        if (haveaccount == 1):
-            isValidOptionSelected = True
-            login()
-        elif(haveaccount == 2):
-            isValidOptionSelected = True
-            #register()
-            print(register())
-        else:
-            print('you have selected invalid option')
+    if (haveaccount == 1):
+        isValidOptionSelected = True
+        login()
+    elif(haveaccount == 2):
+        isValidOptionSelected = True
+        #register()
+        print(register())
+    else:
+        print('you have selected invalid option')
+
+    init()
 
 def login():
-    print ('login to your account')
-    accountnumber =input('insert your account number \n')
+    print ('*******login********')
+    accountnumberfromuser =int(input('insert your account number \n'))
     password = input('input your password \n')
     
+    for accountnumber,userdetails in database.items():
+        if(accountnumber == accountnumberfromuser):
+            if(userdetails[3] == password):
+                bankoperation(userdetails)
+        
+        print("invalid account number")
+        login()
 
-
-    bankoperation()
+    
 
 def register():
     print('***Register***')
@@ -51,21 +57,41 @@ def register():
     database[accountnumber] = [ first_name, last_name, email, password ]
 
     print('Your account has been created')
-
-    return database
+    print ('your account number is: %d' % accountnumber)
+    print ('make sure to keep it safe')
+    print ('======== ========= =========== ======')
+    #return database
     login()
 
-def bankoperation():
-    print('1. withdrawal')
-    print('2. deposit')
-    print('3. transfers')
-    print('4. careers')
+def bankoperation(user):
+    print('welcome %s %s '% (user[0], user[1]))
+    selectedoption = int(input('select an option (1)deposit (2)withdrawal (3) logout (4)exit \n'))
+    if(selectedoption == 1):
+        depositoperation()
+    elif(selectedoption == 2):
+        withdrawaloperation()
+    elif(selectedoption ==3):
+        logout()
+    elif(selectedoption ==4):
+        exit()
+    else:
+        print('invalid option selected')
+    bankoperation(user)
+
+def withdrawaloperation():
+    print('withdrawal')
+
+def depositoperation():
+    print('deposit operation')
 
 def generationaccountnumber():
-    
     print('generating account number')
+    
     return random.randrange(1111111111,9999999999)
     print(generationaccountnumber())
+
+def logout():
+    login()
 
 ### actual banking system ###
 
